1c65392340d025546c4affabc31158017242f51d
jacobshiff <jacobshiff@gmail.com>
2016-05-22 15:43:01 -0400

    v2

--- db/migrate/20160522194128_correct_reaction_type_column.rb
+++ db/migrate/20160522194128_correct_reaction_type_column.rb
@@ -1,4 +1,4 @@
-class ChangeReactionTypeColumn < ActiveRecord::Migration
+class CorrectReactionTypeColumn < ActiveRecord::Migration
   def change
     rename_column :reactions, :type, :reaction_type
   end
