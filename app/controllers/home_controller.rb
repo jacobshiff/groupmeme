6f395b434eddfbf0eae7f26b4b100cf2d1ac0b7a
xristocodos <xristojobos@gmail.com>
2016-05-25 14:30:46 -0400

    starting over w a clean slate

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
 
   def index
+
     if current_user   #if logged in already
       if current_user.groups.count == 1
         redirect_to(group_path(current_user.groups.first.group_slug))
@@ -9,7 +10,8 @@ class HomeController < ApplicationController
       end
 
     else #if NOT logged in
-  
+      # render :layout => false
+      render layout: false
       render file: "/vendor/index.html"
     end
 
