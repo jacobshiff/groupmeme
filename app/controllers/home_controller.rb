b0b85f04a717af4fd51dd575cab9f01f3227f00e
xristocodos <xristojobos@gmail.com>
2016-05-26 10:56:13 -0400

    fake indent on user show page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
   def index
     if current_user
       if current_user.groups.count == 1
+        binding.pry
         redirect_to(group_path(current_user.groups.first.group_slug))
       else
         redirect_to(groups_path)
