188c83736a33ae89eafcb97b5c1724ef3932e695
xristocodos <xristojobos@gmail.com>
2016-05-26 12:01:25 -0400

    got profile show, just trying to get profile images to overwrite

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,15 +7,22 @@ class UsersController < ApplicationController
   def edit
   end
 
-  def update
-  end
-
   def destroy
     current_user.destroy_memberships
     current_user.destroy
   end
 
   def update
+    binding.pry
+    @user = User.find_by(username: params[:username])
+    @user.update(user_params)
+    redirect_to user_path(@user.username)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username,:avatar)
   end
 
 end
