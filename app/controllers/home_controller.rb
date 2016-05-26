e52b388e60564d4ea8f76685c1be8552d6c1cfb8
jacobshiff <jacobshiff@gmail.com>
2016-05-26 11:40:33 -0400

    Tweaks

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,6 @@ class HomeController < ApplicationController
   def index
     if current_user
       if current_user.groups.count == 1
-        binding.pry
         redirect_to(group_path(current_user.groups.first.group_slug))
       else
         redirect_to(groups_path)
