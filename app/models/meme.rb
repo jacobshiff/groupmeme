1acb48fccdec1a3042b324bda4def04ec1a21ccc
jacobshiff <jacobshiff@gmail.com>
2016-05-30 15:13:42 -0400

    Added thumbnail post-processing for faster loadign on index

--- app/models/meme.rb
+++ app/models/meme.rb
@@ -7,7 +7,7 @@ class Meme < ActiveRecord::Base
   belongs_to :group
 
   #paperclip validations; must include for upload
-  has_attached_file :image
+  has_attached_file :image, styles: { thumb: ["300x300"] }
   validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 
   def update_reactions(user)
