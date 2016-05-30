22009494901c0b2532088ecb8468e42cde09631a
jacobshiff <jacobshiff@gmail.com>
2016-05-30 10:52:40 -0400

    Added cheap man caching for current_group and current_user

--- app/controllers/memes_controller.rb
+++ app/controllers/memes_controller.rb
@@ -8,31 +8,29 @@ class MemesController < ApplicationController
     before_action :require_login_and_access
 
   def index
-    # group_slug = params[:group_slug]
-    # @group = Group.find_by(group_slug: group_slug)
-    @memes = Meme.where(group: @group).reverse
+    @memes = Meme.where(group: @current_group).reverse
 
     #Toodoo: REFACTOR ME INTO HELPER METHOD FOR SORTING TYPE CHOOSER
     #params[:sort]  #viral, or time, or rising   /memes/by/[x]
 
-    if params[:sort]   #how do we wish to mutate-sort our localvar, @memes?
-      #if it exists
-      case (params[:sort])
-      when "viral"
-        #sort virally
-        @memes = @memes.sort_by {|meme| - meme.reactions.count}
-      when "newest"
-        #sort chrono
-        @memes = @memes.sort_by(&:created_at).reverse
-      when "oldest"
-        @memes = @memes.sort_by(&:created_at)
-      when "rising"
-        #sort rising
-        @memes = @memes.sort_by {|meme| - meme.reactions.where(created_at: (Time.now - 1.hour)..Time.now).count}
-      else
-        flash[:warning] = "What exactly are you trying to do here, pal?"
-      end
-    end
+    # if params[:sort]   #how do we wish to mutate-sort our localvar, @memes?
+    #   #if it exists
+    #   case (params[:sort])
+    #   when "viral"
+    #     #sort virally
+    #     @memes = @memes.sort_by {|meme| - meme.reactions.count}
+    #   when "newest"
+    #     #sort chrono
+    #     @memes = @memes.sort_by(&:created_at).reverse
+    #   when "oldest"
+    #     @memes = @memes.sort_by(&:created_at)
+    #   when "rising"
+    #     #sort rising
+    #     @memes = @memes.sort_by {|meme| - meme.reactions.where(created_at: (Time.now - 1.hour)..Time.now).count}
+    #   else
+    #     flash[:warning] = "What exactly are you trying to do here, pal?"
+    #   end
+    # end
   end
 
   def show
@@ -115,7 +113,8 @@ class MemesController < ApplicationController
   end
 
   def set_group
-    @group = Group.find_by(group_slug: params[:group_slug])
+    # @group = Group.find_by(group_slug: params[:group_slug])
+    @current_group ||= Group.find_by(group_slug: params[:group_slug]) if params[:group_slug]
   end
 
   def meme_params
