ada084e8fdec39428348ddb81d7a3eeb9e461153
jacobshiff <jacobshiff@gmail.com>
2016-05-31 14:15:27 -0400

    V1 is working

--- app/assets/javascripts/file_uploader.js
+++ app/assets/javascripts/file_uploader.js
@@ -7,15 +7,12 @@ var preview = document.getElementById('preview');
 
 var form = document.getElementById('form');
 
-$(function() {
-  $('#pictureInput').on('change', function(event) {
-    var files = event.target.files;
-    var file = files[0]
-    // debugger  
-    // var image = files[0]
-    // var blob = new Blob(files);
-    // var blobURL = URL.createObjectURL(blob);
-    // here's the file size
+function processfile(file) {  
+    if( !( /image/i ).test( file.type ) )
+        {
+            alert( "File "+ file.name +" is not an image." );
+            return false;
+        }
     var reader = new FileReader();
     reader.readAsArrayBuffer(file);
     reader.onload = function(event) {
@@ -55,7 +52,51 @@ function readfiles(files) {
     // TODO remove the previous hidden inputs if user selects other files
 }
 
+fileinput.onchange = function(){
+  if ( !( window.File && window.FileReader && window.FileList && window.Blob ) ) {
+    alert('The File APIs are not fully supported in this browser.');
+    return false;
+    }
+  readfiles(fileinput.files);
+}
 
+// === RESIZE ====
+
+function resizeMe(img) {
+  
+  var canvas = document.createElement('canvas');
+
+  var width = img.width;
+  var height = img.height;
+
+  // calculate the width and height, constraining the proportions
+  if (width > height) {
+    if (width > max_width) {
+      //height *= max_width / width;
+      height = Math.round(height *= max_width / width);
+      width = max_width;
+    }
+  } else {
+    if (height > max_height) {
+      //width *= max_height / height;
+      width = Math.round(width *= max_height / height);
+      height = max_height;
+    }
+  }
+  
+  // resize the canvas and draw the image data into it
+  canvas.width = width;
+  canvas.height = height;
+  var ctx = canvas.getContext("2d");
+  ctx.drawImage(img, 0, 0, width, height);
+  
+  preview.appendChild(canvas); // do the actual resized preview
+  
+  return canvas.toDataURL("image/gif",0.7); // get the data from canvas as 70% JPG (can be also PNG, etc.)
+
+}
+
+$(function() {
   $('textarea.meme-text').on('change', function(event){
     var top_text = $('textarea#top-text').val()
     var bottom_text = $('textarea#bottom-text').val()
@@ -72,47 +113,12 @@ function readfiles(files) {
     $('input#meme-title-input').val(title)
   })
 
-  function drawCanvas(img) {
-    var max_width = 600;
-    var max_height = 600;
-    var width = img.width;
-    var height = img.height;
-    var canvas = document.getElementById("canvas");
-    var ctx = canvas.getContext("2d");
-
-    // calculate the width and height, constraining the proportions
-    if (width > height) {
-      if (width > max_width) {
-        //height *= max_width / width;
-        height = Math.round(height *= max_width / width);
-        width = max_width;
-      }
-    } else {
-      if (height > max_height) {
-        //width *= max_height / height;
-        width = Math.round(width *= max_height / height);
-        height = max_height;
-      }
-    }
-  
-    // resize the canvas and draw the image data into it
-    canvas.width = width;
-    canvas.height = height;
-    // ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(img, 0, 0, width, height);
-    return canvas.toDataURL("image/jpeg",0.7);
-    // ctx.drawImage(image, 33, 90, 104, 124, 21, 20, 87, 104);
-  }
-
-
-
 // Prevent enter from submitting form on tag field
 $("#tag-autocomplete").bind("keypress", function (e) {
     if (e.keyCode == 13) {
         return false;
     }
 });
-
 
   // progress bar
   $('#meme-upload-submit').on('click', function(event){    
