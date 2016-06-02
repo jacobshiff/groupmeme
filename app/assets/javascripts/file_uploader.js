ff03e5ad2a017be620c11d2ac780f485951a2e27
jacobshiff <jacobshiff@gmail.com>
2016-06-02 16:57:06 -0400

    fixed image uploader bug

--- app/assets/javascripts/file_uploader.js
+++ app/assets/javascripts/file_uploader.js
@@ -28,5 +28,4 @@ $(function() {
     var progress_bar_html = '<div id="progressDiv" class="progress"><div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div></div>'
     $('#progressDivtarget').html(progress_bar_html)
   })
-
-});
+})
